be47b9497e441c78e46eb103d038ac132ae90209
vikenty19 <vikenty.plakhov@testpro.io>
2025-06-17 17:29:36 -0400

    .

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
         </a>
       </header>
       <p>
-          Application version: {process.env.REACT_APP_VERSION}
+          Application version: 1
       </p>
     </div>
   );
